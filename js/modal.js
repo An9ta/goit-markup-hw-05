15af9ae1283c22fe4efe8ddc4044a6618771bb59
An9ta <147629509+An9ta@users.noreply.github.com>
2023-12-16 15:32:14 +0100

    6

--- js/modal.js
+++ js/modal.js
@@ -5,10 +5,9 @@
     modal: document.querySelector("[data-modal]"),
   };
 
-  refs.openModalBtn.addEventListener("click", toggleModal);
-  refs.closeModalBtn.addEventListener("click", toggleModal);
-
   function toggleModal() {
+    refs.openModalBtn.addEventListener("click", toggleModal);
+    refs.closeModalBtn.addEventListener("click", toggleModal);
     refs.modal.classList.toggle("is-hidden");
   }
 })();
